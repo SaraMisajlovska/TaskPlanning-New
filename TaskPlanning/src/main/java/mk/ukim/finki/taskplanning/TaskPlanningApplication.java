0e85ef95436c50afd259b8e9e48b4922d29d362e
SaraMisajlovska <smisajlovska@gmail.com>
2022-02-12 12:10:43 +0100

    added spring boot project

---
+++ TaskPlanning/src/main/java/mk/ukim/finki/taskplanning/TaskPlanningApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.taskplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskPlanningApplication.class, args);
+    }
+
+}
---
+++ TaskPlanning/src/test/java/mk/ukim/finki/taskplanning/TaskPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.taskplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
